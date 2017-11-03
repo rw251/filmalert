d1da9203a772f0bea4078c03d995397293a7a0f3
Richard Williams <richard.williams2@manchester.ac.uk>
2017-11-03 11:35:09 +0000

    Removed config

--- app/private/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("CONST_DB_HOST",getenv('FILMALERT_DB_HOST'));
-define("CONST_DB_USERNAME",getenv('FILMALERT_DB_USER'));
-define("CONST_DB_PASSWORD",getenv('FILMALERT_DB_PASS'));
-define("CONST_DB_NAME",getenv('FILMALERT_DB_NAME'));
-?>
