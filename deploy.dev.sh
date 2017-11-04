5208905c338f67454076615d9679f21c82ebfaa0
Richard Williams <richard.williams2@manchester.ac.uk>
2017-11-04 07:46:53 +0000

    Removed files from old version of site

--- deploy.dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-grunt dev
-lftp -e "mirror -R /home/ubuntu/workspace/dist/public_html/ /public_html  && exit" -u $FTP_USER,$FTP_PASS $FTP_URL 
--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-grunt
-lftp -d -e "mirror -R /home/ubuntu/workspace/dist/public_html/ /public_html  && exit" -u $FTP_USER,$FTP_PASS $FTP_URL 
