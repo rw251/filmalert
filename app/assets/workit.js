d29dd379ec4138f9c712f998c1b9de2fd497ff37
Richard Williams <richard.williams@manchester.ac.uk>
2019-06-27 15:10:55 +0100

    Removed old brunch app

--- app/assets/workit.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('mysite-static-v3').then(cache => cache.addAll([
-      DIGEST(app.css),
-        // etc
-    ])),
-  );
-});
--- app/private/config.example.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("CONST_DB_HOST","localhost");
-define("CONST_DB_USERNAME","username");
-define("CONST_DB_PASSWORD","password");
-define("CONST_DB_NAME","db");
-?>
