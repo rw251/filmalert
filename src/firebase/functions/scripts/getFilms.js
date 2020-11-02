cd67ebda1285c1bec4dce2f9f7925257a76e0ffd
Richard Williams <richard.williams@manchester.ac.uk>
2020-11-02 21:11:58 +0000

    Update way films are collected

--- src/firebase/functions/scripts/getFilms.js
+++ src/firebase/functions/scripts/getFilms.js
@@ -1,56 +1,107 @@
 const $ = require('cheerio');
 const rp = require('request-promise');
 
-const filmUrl = 'https://nextfilm.uk/';
-
-const validateFilm = (film, date) => {
-  if (!film.imdb || film.imdb[0] !== 't' || film.imdb[1] !== 't') return false;
-  if (!film.time || film.time.indexOf('-') < 0) return false;
-  film.time = `${date} ${film.time.split('-')[0].replace(/ /g, '')}:00`;
-
-  // Check it's still today
-  const actualDateInMS = (new Date(date)).getTime();
-  const actualTimeInMS = (new Date(film.time)).getTime();
-  if(Math.abs(actualDateInMS - actualTimeInMS) > 48*60*60*1000) return false;
-
-  // Check the year is a year
-  if(!/^[12][0-9]{3}$/.test(film.year)) film.year = "????";
-
-  film.imdb = film.imdb.slice(2);
-  return film;
+const services = {
+  nextfilm: 'nextfilm',
+  tvfilms: 'tvfilms',
 };
 
-const getFilmFromHtml = (node, date) => {
-  try {
-    const film = {
-      channel: $('.chanbox img', node).attr('title'),
-      title: $('a.title', node).text(),
-      year: $('i', node)[0].childNodes[0].data.substr(1, 4),
-      time: $('span.time strong', node).text(),
-      imdb: $('.imdb a.ib:contains(IMDb)', node).attr('href').split('/')[4],
-    };
-    return validateFilm(film, date);
-  } catch (e) {
-    return false;
-  }
+// Change this to use a different service
+const service = services.tvfilms;
+
+const url = {
+  nextfilm: 'https://nextfilm.uk/',
+  tvfilms: 'https://www.tv-films.co.uk/',
+};
+
+const request = {
+  nextfilm: (id) =>({
+    uri: `${url.nextfilm}?id=${id}`,
+    headers: {
+      'User-Agent': 'Request-Promise',
+      Host: 'nextfilm.uk',
+    },
+  }),
+  tvfilms: () => url.tvfilms,
+};
+
+const filmClass = {
+  nextfilm: '.listentry',
+  tvfilms: '.Freeview',
+};
+
+const validate = {
+  nextfilm: (film, date) => {
+    if (!film.imdb || film.imdb[0] !== 't' || film.imdb[1] !== 't') return false;
+    if (!film.time || film.time.indexOf('-') < 0) return false;
+    film.time = `${date} ${film.time.split('-')[0].replace(/ /g, '')}:00`;
+  
+    // Check it's still today
+    const actualDateInMS = (new Date(date)).getTime();
+    const actualTimeInMS = (new Date(film.time)).getTime();
+    if(Math.abs(actualDateInMS - actualTimeInMS) > 48*60*60*1000) return false;
+  
+    // Check the year is a year
+    if(!/^[12][0-9]{3}$/.test(film.year)) film.year = "????";
+  
+    film.imdb = film.imdb.slice(2);
+    return film;
+  },
+  tvfilms: (film) => {
+    if (!film.imdb || film.imdb[0] !== 't' || film.imdb[1] !== 't') return false;
+    if (!film.time) return false;
+    film.time = new Date(film.time).toISOString().replace(/[TZ]/g,' ').substr(0,19);
+  
+    // Check the year is a year
+    if(!/^[12][0-9]{3}$/.test(film.year)) film.year = "????";
+  
+    film.imdb = film.imdb.slice(2);
+    return film;
+  },
+};
+
+const getFilm = {
+  nextfilm: (node, date) => {
+    try {
+      const film = {
+        channel: $('.chanbox img', node).attr('title'),
+        title: $('a.title', node).text(),
+        year: $('i', node)[0].childNodes[0].data.substr(1, 4),
+        time: $('span.time strong', node).text(),
+        imdb: $('.imdb a.ib:contains(IMDb)', node).attr('href').split('/')[4],
+      };
+      return validate.nextfilm(film, date);
+    } catch (e) {
+      return false;
+    }
+  },
+  tvfilms: (node) => {
+    try {
+      const film = {
+        channel: $(node).attr('channel'),
+        title: $(node).attr('title'),
+        year: $(node).attr('year'),
+        time: $(node).attr('showtime'),
+        imdb: $('.imdbRatingPlugin', node).attr('data-title'),
+      };
+      return validate.tvfilms(film);
+    } catch (e) {
+      return false;
+    }
+  },
 };
 
 const filmObj = {
 
 };
 
-const getFilms = (id, date) => rp({
-  uri: `${filmUrl}?id=${id}`,
-  headers: {
-    'User-Agent': 'Request-Promise',
-    Host: 'nextfilm.uk',
-  },
-})
-  .then(html => $('.listentry', html))
-  .then(data => data.map((x, y) => getFilmFromHtml(y, date)))
+const getFilms = (id, date) => rp(request[service](id))
+  .then(html => $(filmClass[service], html))
+  .then(data => data.map((x, y) => getFilm[service](y, date)))
   .then(films => Array.from(films))
   .then(films => films.filter(x => x))
   .then(films => films.forEach((film) => {
+    console.log(film);
     if (!filmObj[film.imdb] || film.time > filmObj[film.imdb].time) {
       filmObj[film.imdb] = film;
     }
@@ -124,9 +175,6 @@ const filmModule = (admin, config) => {
 };
 
 module.exports = filmModule;
-
-
-
 
 // tidyFilms(nowISO)
 //   .then(() => Promise.all([
