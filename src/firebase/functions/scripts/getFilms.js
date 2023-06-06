789463efaf9f60e7f90d30ba76ff56745a07c2cc
Richard Williams <1234richardwilliams@gmail.com>
2023-06-06 14:33:21 +0100

    Fix to new api

--- src/firebase/functions/scripts/getFilms.js
+++ src/firebase/functions/scripts/getFilms.js
@@ -14,7 +14,10 @@ const service = services.tvfilmapi;
 const url = {
   nextfilm: 'https://nextfilm.uk/',
   tvfilms: 'https://www.tv-films.co.uk/',
-  tvfilmapi: ['https://www.tv-films.co.uk/api/shows/','https://www.tv-films.co.uk/api/shows2/'],
+  tvfilmapi: [
+    'https://www.tv-films.co.uk/api/shows/',
+    'https://www.tv-films.co.uk/api/shows2/',
+  ],
 };
 
 const request = {
@@ -26,10 +29,10 @@ const request = {
     },
   }),
   tvfilms: () => url.tvfilms,
-  tvfilmapi: (id, backup) => (backup
-    ? { uri: url.tvfilmapi[1], json: true }
-    : { uri: url.tvfilmapi[0], json: true }
-  ),
+  tvfilmapi: (id, backup) =>
+    backup
+      ? { uri: url.tvfilmapi[1], json: true }
+      : { uri: url.tvfilmapi[0], json: true },
 };
 
 const filmClass = {
@@ -39,50 +42,60 @@ const filmClass = {
 
 const validate = {
   nextfilm: (film, date) => {
-    if (!film.imdb || film.imdb[0] !== 't' || film.imdb[1] !== 't') return false;
+    if (!film.imdb || film.imdb[0] !== 't' || film.imdb[1] !== 't')
+      return false;
     if (!film.time || film.time.indexOf('-') < 0) return false;
     film.time = `${date} ${film.time.split('-')[0].replace(/ /g, '')}:00`;
 
     // Check it's still today
-    const actualDateInMS = (new Date(date)).getTime();
-    const actualTimeInMS = (new Date(film.time)).getTime();
-    if (Math.abs(actualDateInMS - actualTimeInMS) > 48 * 60 * 60 * 1000) return false;
+    const actualDateInMS = new Date(date).getTime();
+    const actualTimeInMS = new Date(film.time).getTime();
+    if (Math.abs(actualDateInMS - actualTimeInMS) > 48 * 60 * 60 * 1000)
+      return false;
 
     // Check the year is a year
-    if (!/^[12][0-9]{3}$/.test(film.year)) film.year = "????";
+    if (!/^[12][0-9]{3}$/.test(film.year)) film.year = '????';
 
     film.imdb = film.imdb.slice(2);
     return film;
   },
   tvfilms: (film) => {
-    if (!film.imdb || film.imdb[0] !== 't' || film.imdb[1] !== 't') return false;
+    if (!film.imdb || film.imdb[0] !== 't' || film.imdb[1] !== 't')
+      return false;
     if (!film.time) return false;
-    film.time = new Date(film.time).toISOString().replace(/[TZ]/g, ' ').substr(0, 19);
+    film.time = new Date(film.time)
+      .toISOString()
+      .replace(/[TZ]/g, ' ')
+      .substr(0, 19);
 
     // Check the year is a year
-    if (!/^[12][0-9]{3}$/.test(film.year)) film.year = "????";
+    if (!/^[12][0-9]{3}$/.test(film.year)) film.year = '????';
 
     film.imdb = film.imdb.slice(2);
     return film;
   },
   tvfilmapi: (film) => {
-    if (!film.imdb || film.imdb[0] !== 't' || film.imdb[1] !== 't') return false;
+    if (!film.imdb || film.imdb[0] !== 't' || film.imdb[1] !== 't')
+      return false;
     if (!film.time) return false;
-    film.time = new Date(film.time).toISOString().replace(/[TZ]/g, ' ').substr(0, 19);
+    film.time = new Date(film.time)
+      .toISOString()
+      .replace(/[TZ]/g, ' ')
+      .substr(0, 19);
 
     // Check the year is a year
-    if (!/^[12][0-9]{3}$/.test(film.year)) film.year = "????";
+    if (!/^[12][0-9]{3}$/.test(film.year)) film.year = '????';
 
     film.imdb = film.imdb.slice(2);
     return film;
-  }
+  },
 };
 
 const isHtml = {
   nextfilm: true,
   tvfilms: true,
   tvfilmapi: false,
-}
+};
 
 const getFilm = {
   nextfilm: (node, date) => {
@@ -115,9 +128,7 @@ const getFilm = {
   },
 };
 
-const filmObj = {
-
-};
+const filmObj = {};
 
 const processHtml = {
   nextfilm: (html) => {
@@ -137,44 +148,53 @@ const processJson = {
     json.channels.forEach((channel) => {
       channels[channel.id] = channel.name;
     });
-    return json.shows.map((show) => {
-      const film = {
-        channel: show.channel_id ? channels[show.channel_id] : show.link, // now includes iplayer shows
-        title: json.films[show.film_id].title,
-        year: json.films[show.film_id].year,
-        time: show.time,
-        imdb: json.films[show.film_id].imdb,
-      };
-      return validate.tvfilmapi(film);
-    });
+    return json.shows
+      .map((show) => {
+        if (!json.films[show.f]) return false;
+        const film = {
+          channel: show.channel_id ? channels[show.channel_id] : show.link, // now includes iplayer shows
+          title: json.films[show.f].title,
+          year: json.films[show.f].year,
+          time: show.time,
+          imdb: json.films[show.f].imdb,
+        };
+        return validate.tvfilmapi(film);
+      })
+      .filter(Boolean);
   },
 };
 
-const getFilms = (id, date) => rp(request[service](id))
-  .catch(() => {
-    // Try the backup url
-    return rp(request[service](id, true));
-  })
-  .then(htmlOrJSON => {
-    if (isHtml[service]) {
-      return processHtml[service](htmlOrJSON);
-    } else {
-      return processJson[service](htmlOrJSON)
-    }
-  })
-  .then(films => Array.from(films))
-  .then(films => films.filter(x => x))
-  .then(films => films.forEach((film) => {
-    if (!filmObj[film.imdb] || film.time > filmObj[film.imdb].time) {
-      filmObj[film.imdb] = film;
-    }
-  }));
+const getFilms = (id, date) =>
+  rp(request[service](id))
+    .catch(() => {
+      // Try the backup url
+      return rp(request[service](id, true));
+    })
+    .then((htmlOrJSON) => {
+      if (isHtml[service]) {
+        return processHtml[service](htmlOrJSON);
+      } else {
+        return processJson[service](htmlOrJSON);
+      }
+    })
+    .then((films) => Array.from(films))
+    .then((films) => films.filter((x) => x))
+    .then((films) =>
+      films.forEach((film) => {
+        if (!filmObj[film.imdb] || film.time > filmObj[film.imdb].time) {
+          filmObj[film.imdb] = film;
+        }
+      })
+    );
 
 const timeToRemoveFrom = () => {
   const now = new Date();
   now.setHours(now.getHours() - 4);
-  return now.toISOString().split("T").reduce((date, time) => date + ' ' + time.substr(0, 5))
-}
+  return now
+    .toISOString()
+    .split('T')
+    .reduce((date, time) => date + ' ' + time.substr(0, 5));
+};
 
 const filmModule = (admin, config) => {
   /**
@@ -183,30 +203,32 @@ const filmModule = (admin, config) => {
    */
   const tidyFilms = () => {
     const batch = admin.firestore().batch();
-    return admin.firestore()
+    return admin
+      .firestore()
       .collection(config.collections.films)
-      .where("time", "<", timeToRemoveFrom())
+      .where('time', '<', timeToRemoveFrom())
       .get()
       .then((snapshot) => {
-        snapshot.docs.forEach(doc => {
+        snapshot.docs.forEach((doc) => {
           if (!doc.data().users || Object.keys(doc.data().users).length === 0)
             batch.delete(doc.ref);
         });
         return batch.commit();
       });
-  }
-
+  };
 
   const insertNewFilms = async (films) => {
-
     const batchArray = [];
     batchArray.push(admin.firestore().batch());
     let operationCounter = 0;
     let batchIndex = 0;
 
     // let batch = admin.firestore().batch();
-    films.forEach(x => {
-      let newFilm = admin.firestore().collection(config.collections.films).doc(x.imdb);
+    films.forEach((x) => {
+      let newFilm = admin
+        .firestore()
+        .collection(config.collections.films)
+        .doc(x.imdb);
       batchArray[batchIndex].set(newFilm, x, { merge: true });
       operationCounter++;
 
@@ -216,7 +238,11 @@ const filmModule = (admin, config) => {
         operationCounter = 0;
       }
     });
-    console.log(`${films.length} films inserting in ${batchArray.length} batch${batchArray.length!==1 ? 'es':''}...`);
+    console.log(
+      `${films.length} films inserting in ${batchArray.length} batch${
+        batchArray.length !== 1 ? 'es' : ''
+      }...`
+    );
     batchIndex = 1;
     for (const batch of batchArray) {
       console.log(`Batch ${batchIndex} starting...`);
@@ -228,9 +254,7 @@ const filmModule = (admin, config) => {
   };
 
   return {
-
     getFilmsCron: (context) => {
-
       const now = new Date();
       const nowISO = now.toISOString();
       const nowAsShortDate = now.toISOString().substr(0, 10);
@@ -239,21 +263,25 @@ const filmModule = (admin, config) => {
       const tomorrowAsShortDate = tomorrow.toISOString().substr(0, 10);
       const dayAfterTomorrow = new Date();
       dayAfterTomorrow.setDate(dayAfterTomorrow.getDate() + 2);
-      const dayAfterTomorrowAsShortDate = dayAfterTomorrow.toISOString().substr(0, 10);
+      const dayAfterTomorrowAsShortDate = dayAfterTomorrow
+        .toISOString()
+        .substr(0, 10);
 
       return tidyFilms()
-        .then(() => Promise.all([
-          getFilms(0, nowAsShortDate),
-          getFilms(1, tomorrowAsShortDate),
-          getFilms(2, dayAfterTomorrowAsShortDate),
-        ]))
-        .then(() => Object.keys(filmObj).map(id => filmObj[id]))
-        .then(films => insertNewFilms(films))
-        .catch(err => console.log(err))
+        .then(() =>
+          Promise.all([
+            getFilms(0, nowAsShortDate),
+            getFilms(1, tomorrowAsShortDate),
+            getFilms(2, dayAfterTomorrowAsShortDate),
+          ])
+        )
+        .then(() => Object.keys(filmObj).map((id) => filmObj[id]))
+        .then((films) => insertNewFilms(films))
+        .catch((err) => console.log(err))
         .finally(() => {
           console.log('done');
         });
-    }
+    },
   };
 };
 
@@ -273,4 +301,3 @@ module.exports = filmModule;
 //   .then(films => insertNewFilms(films))
 //   .catch(err => console.log(err))
 //   .finally(() => DB.close());
-
