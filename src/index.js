93dd1a8f186bc7e7818dd063b7120ce4e28a3919
Richard Williams <1234richardwilliams@gmail.com>
2023-11-08 09:48:10 +0000

    Changed files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import './client/styles/main.scss';
+import './components/filmList';
+import './components/filmSearchBox';
+import './components/reloadBanner';
+import './components/todoistAuthLink';
+import './components/dropDownMenu';
+import './components/loader';
 
-import './client/components/filmList';
-import './client/components/filmSearchBox';
-import './client/components/reloadBanner';
-import './client/components/todoistAuthLink';
-import './client/components/dropDownMenu';
-import './client/components/loader';
+import './scripts/auth';
 
-import { publish } from './client/scripts/pubsub';
+import { publish } from './scripts/pubsub';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
@@ -15,13 +15,19 @@ if ('serviceWorker' in navigator) {
       publish('NEW_SW_CONTROLLING');
     });
 
-    navigator.serviceWorker.register('/service-worker.js?097jj').then((registration) => {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, (err) => {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+    navigator.serviceWorker.register('/service-worker.js?097jj').then(
+      (registration) => {
+        // Registration was successful
+        console.log(
+          'ServiceWorker registration successful with scope: ',
+          registration.scope
+        );
+      },
+      (err) => {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      }
+    );
   });
   // const wb = new Workbox('/service-worker.js');
   // // Add an event listener to detect when the registered
